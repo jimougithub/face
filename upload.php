5cf16f6d5aa6ecaa7d21ea743d6ea066d5732f3a
Jim OU <jim_mail@163.com>
2018-01-27 18:55:40 +0800

    updated

--- upload.php
+++ upload.php
@@ -1,19 +1,28 @@
 <?php
-$file = $_FILES['file'];
-$name = $file['file_upload'];							//File name
-$type = strtolower(substr($name,strrpos($name,'.')+1)); //File type
-$allow_type = array('jpg','jpeg','gif','png'); 			//Define accept file types
-if(!in_array($type, $allow_type)){
-  return;												//Upload file type not accept
+$file_size=$_FILES['file_upload']['size'];
+if($file_size>10*1024*1024) {
+    echo "<script>alert('Upload failed: file too big!');window.location='./';</script>";
+    exit();  
 }
-if(!is_uploaded_file($file['tmp_name'])){
-  return ;												//Not upload through HTTP POST
+
+$file_type=$_FILES['file_upload']['type'];  
+echo $file_type;  
+if($file_type!="image/jpeg" && $file_type!='image/pjpeg') {  
+    echo "<script>alert('Only accept jpg, jpeg, png');window.location='./';</script>";
+    exit();  
 }
-$upload_path = "./temp/";								//Define upload path
-//Moving the uploaded file to right path
-if(move_uploaded_file($file['tmp_name'],$upload_path.$file['name'])){
-  echo "<script>alert('Upload Successfully!');window.location='./';</script>";
+
+if(is_uploaded_file($_FILES['file_upload']['tmp_name'])) {
+	//Move uploaded file to right path
+	$uploaded_file=$_FILES['file_upload']['tmp_name'];
+	$file_true_name=$_FILES['myfile']['name'];
+	$move_to_file="/var/www/html/ai/face/temp/".$file_true_name;
+  if(move_uploaded_file($uploaded_file,$move_to_file)){
+	  echo "<script>alert('Upload Successfully!');window.location='./';</script>";
+	}else{
+	  echo "<script>alert('Upload failed: move failed');window.location='./';</script>";
+	}
 }else{
-  echo "<script>alert('Upload failed!');window.location='./';</script>";
+	echo "<script>alert('Upload failed');window.location='./';</script>";
 }
 ?>
