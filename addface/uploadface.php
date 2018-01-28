3f81860d56320fea2b9c5fd61eeaf4019518ece2
Jim OU <jim_mail@163.com>
2018-01-28 11:26:05 +0800

    add validtion

--- addface/uploadface.php
+++ addface/uploadface.php
@@ -1,21 +1,23 @@
 <?php
 $file_size=$_FILES['file_upload']['size'];
 if($file_size>1024*1024) {
-    echo "<script>alert('Upload failed: file too big!');window.location='./';</script>";
+    echo "<script>alert('Upload failed: file size too big!');window.location='./';</script>";
     exit();  
 }
 
 $file_type=$_FILES['file_upload']['type'];  
 if($file_type!="image/jpeg") {  
-    echo "<script>alert('Only accept jpg, jpeg, png');window.location='./';</script>";
-    exit();  
+    echo "<script>alert('Upload failed: Only accept jpg');window.location='./';</script>";
+    exit();
 }
 if($file_type=="image/jpeg") $file_type=".jpg";
-if($file_type=="image/pjpeg") $file_type=".jpeg";
 
-$new_name = "";
-$match_name = "";
-$match_distance = "";
+$new_name = trim($_POST['username']);
+$new_name = str_replace(" ","_",$new_name);
+if($new_name==""){
+	echo "<script>alert('Upload failed: Please input name of this person');window.location='./';</script>";
+    exit();
+}
 
 if(is_uploaded_file($_FILES['file_upload']['tmp_name'])) {
 	//Move uploaded file to right path
