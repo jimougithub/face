3508d75473075d528713d16800b59b006b867926
Jim OU <jim_mail@163.com>
2018-02-21 20:13:28 +0800

    fix

--- inc/conn.php
+++ inc/conn.php
@@ -43,6 +43,6 @@ if(!function_exists('mysql_pconnect')){
 header("Content-Type: text/html;charset=utf-8");
 date_default_timezone_set('PRC');
 
-$conn=mysql_pconnect($mysql_server_name, $mysql_username,$mysql_password, MYSQL_CLIENT_COMPRESS);
+$conn=mysql_pconnect($mysql_server_name, $mysql_username,$mysql_password);
 mysql_select_db($mysql_database, $conn);
 ?>
