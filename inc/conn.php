5835fd13526b35f8e8b75a5e948d81a8037046bf
Jim OU <jim_mail@163.com>
2018-02-21 20:11:58 +0800

    support php7

--- inc/conn.php
+++ inc/conn.php
@@ -1,6 +1,45 @@
 <?php
 require_once("dbinfo.php");
 
+//support php7
+if(!function_exists('mysql_pconnect')){
+    function mysql_pconnect($dbhost, $dbuser, $dbpass){
+        global $dbname;
+        global $mysqli;
+        $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        return $mysqli;
+    }
+    function mysql_select_db($dbname){
+        global $mysqli;
+        return mysqli_select_db($mysqli,$dbname);
+    }
+    function mysql_fetch_array($result){
+        return mysqli_fetch_array($result);
+    }
+    function mysql_fetch_assoc($result){
+        return mysqli_fetch_assoc($result);
+    }
+    function mysql_fetch_row($result){
+        return mysqli_fetch_row($result);
+        }
+    function mysql_query($query){
+        global $mysqli;
+        return mysqli_query($mysqli,$query);
+    }
+    function mysql_escape_string($data){
+        global $mysqli;
+        return mysqli_real_escape_string($mysqli, $data);
+        //return addslashes(trim($data));
+    }
+    function mysql_real_escape_string($data){
+        return mysql_real_escape_string($data);
+    }
+    function mysql_close(){
+        global $mysqli;
+        return mysqli_close($mysqli);
+    }
+}
+
 header("Content-Type: text/html;charset=utf-8");
 date_default_timezone_set('PRC');
 
